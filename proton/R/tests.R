fe75bacceec83ae63369dfedb4b91b199c07c3ec
pbiecek <przemyslaw.biecek@gmail.com>
2015-11-12 22:48:15 +0100

    full surname

--- proton/R/tests.R
+++ proton/R/tests.R
@@ -3,7 +3,7 @@ texts <- c(
 
 Wstępne rozpoznanie sugeruje, że Jan Niebezpieczny nie dba o bezpieczeństo, przeprowadzimy atak na jego konto.
 
-Zadanie 1: Sprawdź jaki jest login Jana N.
+Zadanie 1: Sprawdź jaki jest login Jana Niebezpiecznego.
 
 Bit ze strony www instytutu pozyskał zbiór danych `pracownicy`, teraz musi znaleźć w nim login Niebiezpiecznego.
 Pomóż mu w tym. Gdy już dowiesz się jaki to login, użyj komendy `proton(action = \"login\", login=\"XYZ\")` gdzie XYZ to login Niebezpiecznego."
