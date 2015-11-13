f854f282abe1de3d5f96b90ce0367dbee00fd6f9
pbiecek <przemyslaw.biecek@gmail.com>
2015-11-13 20:40:27 +0100

    help da danych

---
+++ proton/R/top1000passwords.R
@@ -0,0 +1,12 @@
+#' Wektor 1000 najpopularniejszych haseł.
+#'
+#' Wektor 1000 najczęściej stosowanych haseł.
+#' Posortowana po częstości użycia, pierwsze hasła są często używane.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name top1000passwords
+#' @usage data(top1000passwords)
+#' @format Wektor z 1000 elementami.
+NULL
+
