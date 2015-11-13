0b7d813116f6fdccedf2e597a20df4e5829fd8f6
pbiecek <przemyslaw.biecek@gmail.com>
2015-11-13 20:29:26 +0100

    dokumentacja danych o logowaniach

---
+++ proton/R/logowania.R
@@ -0,0 +1,17 @@
+#' Historia logowań do serwera Proton
+#'
+#' Zbiór danych opisujących historie logowania kto, skąd i kiedy logował się na Protona.
+#' Kolejne kolumny w tym zbiorze danych opisują:
+#'
+#' \itemize{
+#'   \item login. Login użytkownika, który loguje się do Protona.
+#'   \item host. Adres IP komputera, z którego dochodziło do logowania do Protona.
+#'   \item data. Data lofowania do Protona. Wiersze są posortowane po tej kolumnie.
+#' }
+#'
+#' @docType data
+#' @keywords datasets
+#' @name logowania
+#' @usage data(logowania)
+#' @format Ramka danych z 59366 wierszami i 3 kolumnami.
+NULL
