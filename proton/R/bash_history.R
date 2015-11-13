bbc2d3564197b29db0f1757d511efdb363ebc4b9
pbiecek <przemyslaw.biecek@gmail.com>
2015-11-13 20:37:30 +0100

    historia poleceń

---
+++ proton/R/bash_history.R
@@ -0,0 +1,13 @@
+#' Historia ostatnio wykonywanych poleceń.
+#'
+#' Wektor ostatnio wykonywanych poleceń.
+#' Każdy element wektora składa się z nazwy polecenia i argumentów polecenia,
+#' rozdzielonych spacją.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name bash_history
+#' @usage data(bash_history)
+#' @format Wektor z 19913 elementami.
+NULL
+
