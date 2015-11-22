b7190c064da9fa7cb066a7c026455bc59021c78d
pbiecek <przemyslaw.biecek@gmail.com>
2015-11-22 12:44:25 +0100

    small fixes

--- protonENG/R/proton.R
+++ protonENG/R/proton.R
@@ -1,28 +1,28 @@
-#' @title Infiltration of the Proton server
+#' @title The Proton Game
 #'
 #' @description
-#' The \code{proton} function is used for solving problems in the data-based game ,,Infiltration of the proton server''.
+#' The \code{proton} function is used for solving problems in the data-based game ,,The Proton Game''.
 #' Solve four data-based puzzles in order to crack into the Pietraszko's account!
-#' 
+#'
 #' @param ... \code{proton} function is called by different arguments, which vary depending
 #' on a problem that Bit is trying to solve. See \code{Details} in order to learn more about the list of possible arguments.
 #'
-#' @details Every time when some additional hints are needed one should add 
+#' @details Every time when some additional hints are needed one should add
 #' \code{hint=TRUE} argument to the \code{proton} function.
-#' 
-#' In order to get more information about a user on the Proton server  
-#' one should pass \code{action = "login"}, \code{login="XYZ"} arguments 
+#'
+#' In order to get more information about a user on the Proton server
+#' one should pass \code{action = "login"}, \code{login="XYZ"} arguments
 #' to the \code{proton} function.
-#' 
+#'
 #' In order to log into the Proton server one should pass \code{action = "login"},
 #' \code{login="XYZ"}, \code{password="ABC"} arguments to the \code{proton} function.
 #' If the password matches login, then one will receive a message about successful login.
 #'
-#' In order to log into a server different from Proton one should pass 
+#' In order to log into a server different from Proton one should pass
 #' \code{action = "server"}, \code{host="XYZ"} arguments to the \code{proton} function.
 #'
-#' The ,,Infiltration of the Proton server'' game is a free of charge, educational project of the SmarterPoland.pl Foundation.
-#' By transfering a donation on the foundation's account which is 
+#' ,,The Proton Game'' is a free of charge, educational project of the SmarterPoland.pl Foundation.
+#' By transfering a donation on the foundation's account which is
 #' shown on \url{http://smarterpoland.pl/index.php/fundacja/} site, you will help
 #' us to create another educational games. Even a sum of 10 $ will faciliate
 #' creating and maintaining subsequent educational projects!
