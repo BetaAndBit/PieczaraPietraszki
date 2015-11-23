4d55b72234e62afee76a7134a0a4abed5537b1e7
pbiecek <przemyslaw.biecek@gmail.com>
2015-11-23 23:35:51 +0100

    fix in WSKAZOWKA

--- proton/R/proton.R
+++ proton/R/proton.R
@@ -58,7 +58,7 @@ proton <- function(...) {
 
  wsk <- "WSKAZÓWKA"
  if (.Platform$OS.type == 'windows') {
-   wsk <- iconv(wsk, from = "UTF-8", to = "windows-1250")
+   wsk <- iconv(wsk, from = "UTF-8", to = "windows-1250", sub = "byte")
  }
 
  # plain start
